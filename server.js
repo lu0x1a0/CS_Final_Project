0c9ee95981be837b32f90e08e289f0cb756214d0
Arkie Owen <owenarkie@gmail.com>
2021-10-12 16:29:14 +1100

    Made treasure refresh to max number on get

--- server.js
+++ server.js
@@ -109,6 +109,9 @@ function heartbeat() {
 
         }
     }
+
+    // Try generate more treasure
+    gamemap.try_add_treasure()
     
     // Data we send to front end
     io.sockets.emit('heartbeat', {
