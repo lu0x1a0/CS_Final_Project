74c0911ec4ff8cf071134e6ab4bbdc276713d272
Sean Luo <41530060+lu0x1a0@users.noreply.github.com>
2021-10-30 17:29:10 +1100

    neew variable range display

--- public/CannonRender.js
+++ public/CannonRender.js
@@ -1,3 +1,13 @@
+function EllipseRange(theta,a,b,x0,y0) {
+    // solve quadratic
+    var aa = (b*cos(theta))**2+(a*sin(theta))**2
+    var bb = -2* ( b**2*x0*cos(theta)+a**2*y0*sin(theta)  )
+    var cc = (b*x0)**2+(a*y0)**2-(a*b)**2
+    var r1 = (-bb + sqrt(bb**2-4*aa*cc) )/(2*aa)
+    //r2 = (-bb - sqrt(bb**2-4*aa*cc) )/(2*aa)
+    return r1
+}
+
 class CannonRender {
 
     constructor(cannon, player) {
@@ -20,18 +30,22 @@ class CannonRender {
         //arc(this.pos.x,this.pos.y,this.range*2,this.range*2,
         //    this.angle-this.visionfield/2,this.angle+this.visionfield/2)
         ellipseMode(RADIUS)
-        circle(this.pos.x,this.pos.y,this.range)
+        //circle(this.pos.x,this.pos.y,this.range)
         
+        var dirrange = EllipseRange(this.angle-this.player.dir,this.ellipserange.a,this.ellipserange.b,this.ellipserange.x0,this.ellipserange.y0,)
         translate(this.player.vel.x,this.player.vel.y)
+        arc(this.pos.x,this.pos.y,dirrange,dirrange,
+            this.angle-this.visionfield/2,this.angle+this.visionfield/2)
+        
         rotate(this.player.dir)
         // might replace the full ellipse below with an above arc of same size
-        ellipse(
-            this.ellipserange.x0+this.pos.x*cos(-this.player.dir)-this.pos.y*sin(-this.player.dir),
-            this.ellipserange.y0+this.pos.x*sin(-this.player.dir)+this.pos.y*cos(-this.player.dir),
-            this.ellipserange.a,
-            this.ellipserange.b
-        )
-        rotate(-this.player.dir)
+        //ellipse(
+        //    this.ellipserange.x0+this.pos.x*cos(-this.player.dir)-this.pos.y*sin(-this.player.dir),
+        //    this.ellipserange.y0+this.pos.x*sin(-this.player.dir)+this.pos.y*cos(-this.player.dir),
+        //    this.ellipserange.a,
+        //    this.ellipserange.b
+        //)
+        //rotate(-this.player.dir)
         pop()        
     }
 
