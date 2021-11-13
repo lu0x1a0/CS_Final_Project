3f096599301472b829331afd18a13a8fa75be5d0
Sean Luo <41530060+lu0x1a0@users.noreply.github.com>
2021-11-13 12:18:17 +1100

    edited lubuntu.sh

--- lubuntu.sh
+++ lubuntu.sh
@@ -9,4 +9,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 source ~/.bashrc
 command -v nvm
 
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 nvm install --lts
